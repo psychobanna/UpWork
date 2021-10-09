c1e2b76f61508fef20444b60f14b0a19e7904273
sumersinghharawat <sumer@microlent.com>
2021-10-10 01:30:31 +0530

    Done

---
+++ storage/framework/views/14894b9b49782f5fcf50640033bfcd73eeb5c2b8.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /media/microlent/e61eb478-b1d7-4896-bd20-a690762a8be01/Project/laravel/upwork/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
